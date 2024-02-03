112c099f37090550df0d0abeef625a81ea189cfa
Gasper <gasper.kosenina@student.um.si>
2024-02-03 22:06:04 +0100

    add authors image and name to the post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import AddPostForm from "@/components/AddPostForm";
 import { getPosts } from "./db/actions";
-import { UserButton } from "@clerk/nextjs";
-
+import Image from "next/image";
+import { clerkClient } from "@clerk/nextjs";
 
 interface Emoji {
   id: string
   title: string
+  userId: string
+}
+
+interface Author {
+  imageUrl: string
+  firstName: string | null
 }
 
 export default async function Home() {
@@ -15,9 +21,32 @@ export default async function Home() {
       <div className="flex border-b-2 border-black p-7">
         <AddPostForm />
       </div>
-      {emojis.map((e) => (
-        <div key={e.id} className="border-b-2 border-black p-7">{e.title}</div>
+      {emojis.map(async (e) => (
+        <div key={e.id} className="border-b-2 border-black p-7 flex gap-3">
+          <Image
+            src={(await getAuthorsData(e.userId)).imageUrl} // Fix: Pass the imageUrl property of the Author object
+            alt="users profile"
+            width={46}
+            height={46}
+          />
+          <div className="flex flex-col">
+            <div className="flex text-slate-300 gap-1">
+              <span>@{(await getAuthorsData(e.userId)).firstName} </span> · <span> 1 hour ago</span>
+            </div>
+            <span>{e.title}</span>
+          </div>
+        </div>
       ))}
     </div>
   );
+}
+
+
+async function getAuthorsData(userId: string) {
+  const user = await clerkClient.users.getUser(userId)
+  const author: Author = {
+    imageUrl: user.imageUrl,
+    firstName: user.firstName
+  }
+  return author
 }
